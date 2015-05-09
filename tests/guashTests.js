73f6aed00c38507dbb9a3d6f23dbe606655e7117
Tiago Cajahyba <cajahyba@gmail.com>
2015-05-09 18:22:40 -0300

    First commit

---
+++ tests/guashTests.js
@@ -0,0 +1,8 @@
+/*---------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+/// <reference path="../../declares.d.ts" />
+'use strict';
+define(["require", "exports", '../guashDef', 'monaco-testing'], function (require, exports, languageDef, T) {
+    
+});
